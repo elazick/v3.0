a59812c48bd58733ad8cce4abeed2eab9bfb4015
elazick <112683136+elazick@users.noreply.github.com>
2022-12-16 12:25:48 +0200

    main

--- c_make_compile_run.bat
+++
@@ -1,7 +0,0 @@
-cmake CMakeLists.txt
-cmake --install .
-cmake --build .
-copy src\studentai10000.txt Debug\studentai10000.txt
-copy src\studentai10000.txt Debug\studentai100000.txt
-copy src\studentai10000.txt Debug\studentai1000000.txt
-Debug\v1.0.exe
