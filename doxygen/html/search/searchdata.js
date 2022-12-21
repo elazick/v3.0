5ae726fe3de2364d8ea6aeb0d578b3500a978b5b
elazick <112683136+elazick@users.noreply.github.com>
2022-12-21 18:35:31 +0200

    v2.0

---
+++ doxygen/html/search/searchdata.js
@@ -0,0 +1,18 @@
+var indexSectionsWithContent =
+{
+  0: "stz",
+  1: "stz"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes"
+};
+
